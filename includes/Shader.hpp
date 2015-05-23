4768860a5cd042da9a2f35322c56d5746713fa59
Samuel Olivier <s.samuel.olivier@gmail.com>
2015-05-23 07:11:53 -0700

    Added shader handling

--- includes/Shader.hpp
+++ includes/Shader.hpp
@@ -1,11 +1,26 @@
 #ifndef __SHADER__
 #define __SHADER__
 
+#include <GL/glew.h>
+
 class Shader
 {
 public:
-	Shader();
+	Shader(GLenum shaderType);
 	~Shader();
+
+	bool	compileSourceCode(const char* sourceCode);
+	bool	compileSourceFile(const char* filename);
+
+	bool	isValid() const;
+	bool	isCompiled() const;
+	GLenum	shaderType() const;
+	GLuint	shaderId() const;
+
+private:
+	bool	_isCompiled;
+	GLenum	_shaderType;
+	GLuint	_shaderId;
 };
 
 #endif
--- sources/Shader.cpp
+++ sources/Shader.cpp
@@ -1,9 +1,65 @@
 #include "Shader.hpp"
 
-Shader::Shader()
+#include <string>
+#include <fstream>
+
+Shader::Shader(GLenum shaderType) : _isCompiled(false), _shaderType(shaderType)
 {
+	_shaderId = glCreateShader(shaderType);
 }
 
 Shader::~Shader()
 {
+}
+
+bool	Shader::compileSourceCode(const char* sourceCode)
+{
+	if (!this->isValid()) {
+		return false;
+	}
+	_isCompiled = false;
+	glShaderSource(_shaderId, 1, &sourceCode, NULL);
+	glCompileShader(_shaderId);
+	GLint result = GL_FALSE;
+	glGetShaderiv(_shaderId, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		return false;
+	}
+	_isCompiled = true;
+	return true;
+}
+
+bool	Shader::compileSourceFile(const char* filename)
+{
+	std::string		shaderCode;
+	std::ifstream	shaderStream(filename, std::ios::in);
+	if (!shaderStream.is_open()) {
+		return false;
+	}
+	std::string line = "";
+	while (getline(shaderStream, line)) {
+		shaderCode += "\n" + line;
+	}
+	shaderStream.close();
+	return compileSourceCode(shaderCode.c_str());
+}
+
+bool	Shader::isValid() const
+{
+	return _shaderId != 0;
+}
+
+bool	Shader::isCompiled() const
+{
+	return _isCompiled;
+}
+
+GLenum	Shader::shaderType() const
+{
+	return _shaderType;
+}
+
+GLuint	Shader::shaderId() const
+{
+	return _shaderId;
 }
